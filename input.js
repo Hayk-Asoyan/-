388d3d6affa02b3a53523b3ab291286fa86b0f4e
Hayk-Asoyan <haykasoyan90@gmail.com>
2020-06-11 19:16:45 +0400

    index

--- input.js
+++ input.js
@@ -6,13 +6,17 @@ function doc(params) {
 const inp = doc("#inp");
 const but = doc("#but");
 const p = doc("#p");
-
+ 
 inp.addEventListener("input", inputFoo)
 
 function inputFoo(e) {
- 
+    
+     
 but.addEventListener("click", () => {
+  
+  
     p.innerText = e.target.value;
+    
 })
 
    
