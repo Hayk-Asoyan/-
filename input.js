64c0fbf65a38d18c226cd6392494b0e9b1892fe8
Hayk-Asoyan <haykasoyan90@gmail.com>
2020-06-11 13:35:00 +0400

    index

---
+++ input.js
@@ -0,0 +1,19 @@
+function doc(params) {
+    return document.querySelector(params)
+    
+}
+
+const inp = doc("#inp");
+const but = doc("#but");
+const p = doc("#p");
+
+inp.addEventListener("input", inputFoo)
+
+function inputFoo(e) {
+ 
+but.addEventListener("click", () => {
+    p.innerText = e.target.value;
+})
+
+   
+}
